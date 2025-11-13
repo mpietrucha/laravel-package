29739783bd5ba65088fda003c916b97b5fa67ea1
Michał Pietrucha <mpietrucha50@gmail.com>
2025-11-13 20:13:30 +0100

    wip

--- src/Context/Directory.php
+++ src/Context/Directory.php
@@ -2,8 +2,8 @@
 
 namespace Mpietrucha\Laravel\Package\Context;
 
-use Mpietrucha\Laravel\Package\Context;
 use Mpietrucha\Utility\Enumerable\Contracts\EnumerableInterface;
+use Mpietrucha\Utility\Filesystem\Path;
 use Mpietrucha\Utility\Normalizer;
 use Mpietrucha\Utility\Str;
 
@@ -18,11 +18,10 @@ abstract class Directory
     public static function get(EnumerableInterface $backtrace): string
     {
         $file = $backtrace->pipeThrough([
-            fn (EnumerableInterface $backtrace) => $backtrace->reject->internal(Context::class),
-            fn (EnumerableInterface $backtrace) => $backtrace->map->file(),
-            fn (EnumerableInterface $backtrace) => $backtrace->first(),
+            fn (EnumerableInterface $backtrace) => $backtrace->skip(2),
+            fn (EnumerableInterface $backtrace) => $backtrace->firstMap->file(),
         ]) |> Normalizer::string(...);
 
-        return Str::before($file, 'src');
+        return Str::before($file, 'src') |> Path::canonicalize(...);
     }
 }
